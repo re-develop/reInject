5c35fd21bb8da766b6d70adf2bbd990bbed55f12
Florian Klessascheck <florian.klessa@outlook.com>
2020-11-12 13:55:48 +0100

    - added comptaibility to System.IServiceProvider

--- reInject/Implementation/DependencyContainer.cs
+++ reInject/Implementation/DependencyContainer.cs
@@ -9,7 +9,7 @@ namespace ReInject.Implementation
 {
   internal class DependencyContainer : IDependencyContainer
   {
-    public DependencyContainer( string name )
+    public DependencyContainer(string name)
     {
       this.Name = name;
     }
@@ -18,99 +18,104 @@ namespace ReInject.Implementation
     private Dictionary<Type, IDependencyType> _typeCache = new Dictionary<Type, IDependencyType>();
     public string Name { get; private set; }
 
-    public object GetInstance( Type type )
+    public object GetInstance(Type type)
     {
-      if( type == null )
+      if (type == null)
         return null;
 
-      if( IsKnownType( type ) )
+      if (IsKnownType(type))
       {
-        return _typeCache[ type ].Instance;
+        return _typeCache[type].Instance;
       }
       else
       {
-        var dep = new SingleInstanceDependency( this, type );
+        var dep = new SingleInstanceDependency(this, type);
         return dep.Instance;
       }
     }
 
-    public bool IsKnownType( Type type )
+    public bool IsKnownType(Type type)
     {
-      if( type == null )
+      if (type == null)
         return false;
 
-      return _typeCache.ContainsKey( type );
+      return _typeCache.ContainsKey(type);
     }
 
-    public IDependencyContainer Register<I, T>( DependencyStrategy strategy, bool overwrite = false, object instance = null )
+    public IDependencyContainer Register<I, T>(DependencyStrategy strategy, bool overwrite = false, object instance = null)
     {
-      register( typeof( T ), typeof( I ), strategy, overwrite, instance );
+      register(typeof(T), typeof(I), strategy, overwrite, instance);
       return this;
     }
 
-    public IDependencyContainer Register<T>( DependencyStrategy strategy, bool overwrite = false, object instance = null )
+    public IDependencyContainer Register<T>(DependencyStrategy strategy, bool overwrite = false, object instance = null)
     {
-      register( typeof( T ), null, strategy, overwrite, instance );
+      register(typeof(T), null, strategy, overwrite, instance);
       return this;
     }
 
-    private void register( Type type, Type interfaceType, DependencyStrategy strategy, bool overwrite, object instance )
+    private void register(Type type, Type interfaceType, DependencyStrategy strategy, bool overwrite, object instance)
     {
-      if( strategy == DependencyStrategy.AtomicInstance && instance == null )
-        throw new ArgumentException( "AtomicDependencies need an instance provided" );
+      if (strategy == DependencyStrategy.AtomicInstance && instance == null)
+        throw new ArgumentException("AtomicDependencies need an instance provided");
 
-      if( ( IsKnownType( type ) || IsKnownType( interfaceType ) ) && overwrite == false )
+      if ((IsKnownType(type) || IsKnownType(interfaceType)) && overwrite == false)
         return;
 
       IDependencyType dependency = null;
-      switch( strategy )
+      switch (strategy)
       {
         case DependencyStrategy.AtomicInstance:
-          dependency = new AtomicInstanceDependency( this, instance, type, interfaceType );
+          dependency = new AtomicInstanceDependency(this, instance, type, interfaceType);
           break;
 
         case DependencyStrategy.CachedInstance:
-          dependency = new CachedInstanceDependency( this, type, interfaceType );
+          dependency = new CachedInstanceDependency(this, type, interfaceType);
           break;
 
         case DependencyStrategy.NewInstance:
-          dependency = new NewInstanceDependency( this, type, interfaceType );
+          dependency = new NewInstanceDependency(this, type, interfaceType);
           break;
 
         case DependencyStrategy.SingleInstance:
-          dependency = new SingleInstanceDependency( this, type, interfaceType );
+          dependency = new SingleInstanceDependency(this, type, interfaceType);
           break;
 
         default:
-          throw new ArgumentException( "Invalid Strategy", nameof( strategy ) );
+          throw new ArgumentException("Invalid Strategy", nameof(strategy));
       }
 
-      if( dependency != null )
+      if (dependency != null)
       {
-        _typeCache[ type ] = dependency;
-        if( interfaceType != null )
-          _typeCache[ interfaceType ] = dependency;
+        _typeCache[type] = dependency;
+        if (interfaceType != null)
+          _typeCache[interfaceType] = dependency;
       }
     }
 
 
     public T GetInstance<T>(Action<T> onAccess = null)
     {
-      var inst = (T)GetInstance( typeof( T ) );
-      if( onAccess != null )
-        onAccess( inst );
+      var inst = (T)GetInstance(typeof(T));
+      if (onAccess != null)
+        onAccess(inst);
 
       return inst;
     }
 
     public bool IsKnownType<T>()
     {
-      return IsKnownType( typeof( T ) );
+      return IsKnownType(typeof(T));
+    }
+
+    public object GetService(Type type)
+    {
+      return GetInstance(type);
     }
 
     public void Clear()
     {
-      _typeCache.Values.ToList().ForEach( x => x.Clear() );
+      _typeCache.Values.ToList().ForEach(x => x.Clear());
     }
   }
 }
--- reInject/Interfaces/IServiceProvider.cs
+++ reInject/Interfaces/IServiceProvider.cs
@@ -2,7 +2,7 @@
 
 namespace ReInject.Interfaces
 {
-  public interface IServiceProvider
+  public interface IServiceProvider : System.IServiceProvider
   {
     object GetInstance( Type type );
     T GetInstance<T>(Action<T> action = null);
