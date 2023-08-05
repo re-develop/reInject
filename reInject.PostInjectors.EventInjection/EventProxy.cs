df8dce93ae5d5081bcbc07fd65e3ab6c131d8a3d
Aeolin Ferjünnoz <aeolin@aeol.in>
2023-08-05 12:49:28 +0200

    - fixed naming and paths so they match cases

--- reInject.PostInjectors.EventInjection/EventProxy.cs
+++ reInject.PostInjectors.EventInjection/EventProxy.cs
@@ -11,7 +11,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-[assembly: InternalsVisibleTo("ReInjectTests")]
+[assembly: InternalsVisibleTo("ReInject.Tests")]
 namespace ReInject.PostInjectors.EventInjection
 {
   internal class EventProxyTarget 
--- reInject/Properties/AssemblyInfo.cs
+++ reInject/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 
-[assembly: InternalsVisibleTo("reInjectTests")]
+[assembly: InternalsVisibleTo("ReInject.Tests")]
 namespace ReInject.Properties
 {
   public class AssemblyInfo
