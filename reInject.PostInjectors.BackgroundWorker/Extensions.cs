d7abd0dcefc4a4f78b79d61c2401b4a6454973fe
Aeolin Ferjünnoz <aeolin@aeol.in>
2023-08-09 16:17:10 +0200

    - fixed namespace issues

--- reInject.PostInjectors.BackgroundWorker/Extensions.cs
+++ reInject.PostInjectors.BackgroundWorker/Extensions.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace reInject.PostInjectors.BackgroundWorker
+namespace ReInject.PostInjectors.BackgroundWorker
 {
 	public static class Extensions
 	{
--- reInject.Scopes/DependencyContainerScopeFactory.cs
+++ reInject.Scopes/DependencyContainerScopeFactory.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace reInject.Scopes
+namespace ReInject.Scopes
 {
   public class DependencyContainerScopeFactory : IServiceScopeFactory
   {
